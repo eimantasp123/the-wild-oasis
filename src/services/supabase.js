6244721b0882d5c421baad67959ba3cdcc3a4ab8
eimantasp123 <eimantaspetrikas18@gmail.com>
2025-06-02 17:10:31 +0200

    update env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://aaytoakoexofkkutzmul.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFheXRvYWtvZXhvZmtrdXR6bXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NjQ4NDgsImV4cCI6MjA2NDQ0MDg0OH0.5oFi7CwJcbRciIHD4zJCRrKxCIv-rB8lvMmO4n4oy8I";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
